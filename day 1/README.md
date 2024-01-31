6b242281a99b2983095cdfcaa421fceda3971652
Eureka <teklemariameureka@gmail.com>
2024-01-31 03:54:55 +0300

    Add day 1/

---
+++ day 1/README.md
@@ -0,0 +1,5 @@
+Day 1 - Working with Variables in Python to Manage Data
+
+LESSONS:
+    = variable in python
+    = printing output to the console in python
---
+++ day 1/main.py
@@ -0,0 +1,6 @@
+print('Welcome to the band name generator!')
+
+city = input('Where did you grow up: ')
+pet = input('What is your first pet name: ')
+
+print(f'Your band name is {city} + {pet}')
